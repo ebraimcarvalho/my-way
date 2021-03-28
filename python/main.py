63187a75910ba74ea893757cb01d3f7a403cd1cb
Ebraim de França Carvalho Filho <ebraimcarvalho@gmail.com>
2021-03-28 16:15:16 -0300

    lamda functions and reduce

--- python/main.py
+++ python/main.py
@@ -1,3 +1,10 @@
-name = input('Enter with your name: ')
-age = input('Your age: ')
-print(f"Hello {name}. You are {age} years old!")
+# name = input('Enter with your name: ')
+# age = input('Your age: ')
+# print(f"Hello {name}. You are {age} years old!")
+
+
+from functools import reduce
+a = [1,2,3,4,5]
+
+b = reduce(lambda x, y: x + y, a,2)
+print(b)
