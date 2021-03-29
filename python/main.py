ab64fc7968762aa7d50546b9efe34d2ca18ec2d8
Ebraim de França Carvalho Filho <ebraimcarvalho@gmail.com>
2021-03-29 09:53:43 -0300

    pyhon: raise error as class

--- python/main.py
+++ python/main.py
@@ -8,8 +8,29 @@
 # print(f"dale {text}")
 # b = reduce(lambda x, y: x + y, a,1)
 # print(b)
+# try:
+#   a = 5 / 0
+#   print(a)
+# except Exception as e:
+#   print('Error: {}'.format(e))
+
+class ValueTooHigher(Exception):
+  pass
+
+class ValueTooSmaller(Exception):
+  def __init__(self, message, value):
+    self.message = message
+    self.value = value
+
+def test_value(x):
+  if x > 100:
+    raise ValueTooHigher("Value is too bigger")
+  if x < 5:
+    raise ValueTooSmaller("Value is too smaller", x)
+
 try:
-  a = 5 / 0
-  print(a)
-except Exception as e:
-  print(f'Error: {e}')
+  test_value(2)
+except ValueTooHigher as e:
+  print(e)
+except ValueTooSmaller as e:
+  print(f'{e.message}: {e.value}')
