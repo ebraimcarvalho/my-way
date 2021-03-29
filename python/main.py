61733af88ea0e643cc34ca8128baed1cbad6536d
Ebraim de França Carvalho Filho <ebraimcarvalho@gmail.com>
2021-03-29 00:49:14 -0300

    lambda reduce function

--- python/main.py
+++ python/main.py
@@ -2,9 +2,9 @@
 # age = input('Your age: ')
 # print(f"Hello {name}. You are {age} years old!")
 
-
 from functools import reduce
+text = input("Enter text: ")
 a = [1,2,3,4,5]
-
-b = reduce(lambda x, y: x + y, a,2)
+print(f"dale {text}")
+b = reduce(lambda x, y: x + y, a,1)
 print(b)
