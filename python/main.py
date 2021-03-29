ef87ae54a02e1a1f5a902ab83aa5a09669a7d70d
Ebraim de França Carvalho Filho <ebraimcarvalho@gmail.com>
2021-03-29 09:18:00 -0300

    try except

--- python/main.py
+++ python/main.py
@@ -2,9 +2,14 @@
 # age = input('Your age: ')
 # print(f"Hello {name}. You are {age} years old!")
 
-from functools import reduce
-text = input("Enter text: ")
-a = [1,2,3,4,5]
-print(f"dale {text}")
-b = reduce(lambda x, y: x + y, a,1)
-print(b)
+# from functools import reduce
+# text = input("Enter text: ")
+# a = [1,2,3,4,5]
+# print(f"dale {text}")
+# b = reduce(lambda x, y: x + y, a,1)
+# print(b)
+try:
+  a = 5 / 0
+  print(a)
+except Exception as e:
+  print(f'Error: {e}')
