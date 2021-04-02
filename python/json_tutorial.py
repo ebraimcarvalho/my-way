8cfb3fab48d1fa0aaf2f7758ed3eff11cfff9e37
Ebraim de França Carvalho Filho <ebraimcarvalho@gmail.com>
2021-04-02 14:42:39 -0300

    python: json dumps for print out and json dump for write in a file, encoding utf8 and indent with sort keys

---
+++ python/json_tutorial.py
@@ -0,0 +1,15 @@
+import json
+
+person = {
+  'name': 'Ebraim',
+  'age': 29,
+  'city': 'João Pessoa',
+  'hasChildren': False,
+  'titles': ['engineer', 'programmer']
+}
+
+personJSON = json.dumps(person, sort_keys=True, indent=2, ensure_ascii=False)
+print(personJSON)
+
+with open('person.json', 'w', encoding='utf8') as file:
+  json.dump(person, file, ensure_ascii=False, indent=4)
