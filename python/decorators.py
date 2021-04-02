4f77428777589cb539c3980c6a29b88988bb1cbe
Ebraim de França Carvalho Filho <ebraimcarvalho@gmail.com>
2021-04-02 17:53:30 -0300

    python: decorators count calls class

--- python/decorators.py
+++ python/decorators.py
@@ -20,18 +20,36 @@ import functools
 # result = add5(10)
 # print(result)
 
-def repeat(num_times):
-  def decorator_repeat(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-      for _ in range(num_times):
-        result = func(*args, **kwargs)
-      return result
-    return wrapper
-  return decorator_repeat
+# def repeat(num_times):
+#   def decorator_repeat(func):
+#     @functools.wraps(func)
+#     def wrapper(*args, **kwargs):
+#       for _ in range(num_times):
+#         result = func(*args, **kwargs)
+#       return result
+#     return wrapper
+#   return decorator_repeat
 
-@repeat(num_times=3)
-def greet(name):
-  print(f'Hello {name}')
+# @repeat(num_times=3)
+# def greet(name):
+#   print(f'Hello {name}')
 
-greet('Ebraim')
+# greet('Ebraim')
+
+class CountCalls:
+  def __init__(self, func):
+    self.func = func
+    self.num_calls = 0
+
+  def __call__(self, *args, **kwargs):
+    self.num_calls += 1
+    print(f'This is executed {self.num_calls} times')
+    return self.func(*args, **kwargs)
+
+@CountCalls
+def say_hello():
+  print('Hello')
+
+say_hello()
+say_hello()
+say_hello()
