32c5582a905f4057d530c4ce3c9d585e1ca2b1e5
Ebraim de França Carvalho Filho <ebraimcarvalho@gmail.com>
2021-04-03 11:59:49 -0300

    python: change a list inside function

--- python/function_arguments.py
+++ python/function_arguments.py
@@ -15,10 +15,26 @@ my_dict = {'a': 22, 'b': 33, 'c': 44}
 # foo(*my_list)
 # foo(**my_dict)
 
-def func():
-  global number
-  number = 3
+# def func():
+#   global number
+#   number = 3
 
-number = 0
-func()
-print(number)
+# number = 0
+# func()
+# print(number)
+
+# def func(x):
+#   global var
+#   var = 3
+
+# var = 10
+# func(var)
+# print(var)
+
+def changeList(a_list):
+  a_list.append(4)
+  a_list[0] = -100
+
+my_list = [1,2,3]
+changeList(my_list)
+print(my_list)
