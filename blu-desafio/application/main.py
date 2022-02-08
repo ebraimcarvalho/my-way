9a4f23310ea4a350e80c9850b88efbc019c8a226
Ebraim Carvalho <ebraimcarvalho@gmail.com>
2022-02-08 02:08:24 -0300

    update port application

--- blu-desafio/application/main.py
+++ blu-desafio/application/main.py
@@ -66,4 +66,4 @@ def post_data():
     return {'erro': 'Erro ao carregar dados...'}
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5000)
