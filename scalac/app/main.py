aa751ff41e2d44cd1e48191d281532ddfd4d5427
Ebraim Carvalho <ebraimcarvalho@gmail.com>
2022-02-14 11:07:49 -0300

    api working, before refactor

--- scalac/app/main.py
+++ scalac/app/main.py
@@ -1,18 +1,60 @@
 from typing import List, Optional
 from fastapi import FastAPI, HTTPException
+import requests
 from models import User
+from decouple import config
+import itertools
+
+API_KEY = config('GH_TOKEN')
 
 app = FastAPI()
 
 db: List[User] = [
     User(
-        id=1,
-        login="ebraimcarvalho", 
-        org="Accenture", 
-        repo="One Accenture", 
+        login="ebraimcarvalho",
         contributions=5
+    ),
+    User(
+        login="ebraimcarvalho",
+        contributions=10
     )
 ]
+
+def get_request_api_github(link):
+    return requests.get(link, headers={'Authorization': f'token {API_KEY}'})
+
+@app.get('/org/{org_name}/contributors')
+async def get_contributors(org_name: str):
+    link_organization = f"https://api.github.com/users/{org_name}/repos"
+    repos = get_request_api_github(link_organization)
+    if repos.status_code == 200:
+        data = repos.json()
+        for repo in data:
+            link_repo = f"https://api.github.com/repos/{org_name}/{repo['name']}/contributors"
+            contributions = get_request_api_github(link_repo)
+            if contributions.status_code == 200:
+                response = contributions.json()
+                for contrib in response:
+                    db.append(
+                        User(
+                            login=contrib['login'],
+                            contributions=contrib['contributions']
+                        ))
+        mapping = [(user.login, user.contributions) for user in db]
+        mapping.sort(key=lambda x:x[0])
+        result = [(key, sum(num for _, num in value))
+            for key, value in itertools.groupby(mapping, lambda x: x[0])]
+        result.sort(key=lambda x:x[1], reverse=True)
+        output = []
+        for item in result:
+            output.append({
+                'login': item[0],
+                'contributions': item[1]
+            })
+        return output
+    return {
+        'erro': org_name
+    }
 
 @app.get('/')
 async def hello():
--- scalac/app/models.py
+++ scalac/app/models.py
@@ -2,8 +2,5 @@ from pydantic import BaseModel
 from typing import Optional
 
 class User(BaseModel):
-    id : int
     login : str
-    org : str
-    repo : str
     contributions : int
